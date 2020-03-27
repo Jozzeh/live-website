05a756901273d6241bf1d1d020805e5919dc0df2
Jos De Berdt <josdeberdt@MacBook-Pro-van-Jos.local>
2020-03-27 12:50:51 +0100

    Feat - PWA support

---
+++ next.config.js
@@ -0,0 +1,44 @@
+const withOffline = require('next-offline')
+ 
+module.exports = withOffline({
+  workboxOpts: {
+    runtimeCaching: [
+      {
+        urlPattern: /.png$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'png-calls'
+        }
+      },
+      {
+        urlPattern: /.jpeg$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'jpeg-calls'
+        }
+      },
+      {
+        urlPattern: /.jpg$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'jpg-calls'
+        }
+      },
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 7 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        }
+      }
+    ]
+  }
+})
