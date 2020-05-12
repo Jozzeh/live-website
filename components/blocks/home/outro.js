c24a85fccb9ad002d965622435b5f98164c69bfa
Jos De Berdt <josdeberdt@MacBook-Pro-van-Jos.local>
2020-05-12 18:59:10 +0200

    Copy - front copy

--- components/blocks/home/outro.js
+++ components/blocks/home/outro.js
@@ -1,14 +1,27 @@
 const Homeoutro = () => (
   <div id="homeOutro">
-    <div id="homeOutroTitle">
-      But wait, there's more...
-    </div>
+    <div id="homeOutroTitle">But wait, there's more...</div>
     <div id="homeOutroText">
-      <p>I like JavaScript because of the versatility... but I actually like anything front-end related.</p>
-      <p>That includes using HTML & CSS but also on-page SEO, CI/CD pipelines or other languages such as Flutter / Dart.<br/>
-      When I hear or read some rant about CSS and how bad it is; I can't find myself in the reasoning behind it. I believe CSS is fantastic language.<br/>
-      Call me old school but I like to seperate lay-out from behavior...<br/>
-      but hey, the reason I'm still in development after all these years? <strong>Adaptability</strong></p>
+      <p>
+        I like JavaScript because of the versatility... but I actually like
+        anything front-end related.
+      </p>
+      <p>
+        That includes using HTML & CSS but also on-page SEO, CI/CD pipelines or
+        other languages such as Flutter / Dart.
+        <br />
+        When I hear or read some rant about CSS and how bad it is; I can't find
+        myself in the reasoning behind it. I believe CSS is fantastic language.
+        <br />
+        Call me old school but I like to seperate lay-out from behavior...
+        <br />
+        <br />
+        The reason I'm still in development after all these years?
+        <strong>Adaptability</strong>
+        <br />
+        The development industry is moving at a frantic pace, and developers
+        need to keep on learning & adapting... including myself.
+      </p>
     </div>
   </div>
 );
