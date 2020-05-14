c367813a3db0d7c8e1e59dde5cc178ce0cda5f21
Jos De Berdt <josdeberdt@MacBook-Pro-van-Jos.local>
2020-05-14 22:04:34 +0200

    bugfix - hot

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import { useEffect } from "react";
 
 const Index = () => {
   useEffect(() => {
-    if(instanceHero) {
+    if(typeof instanceHero !== "undefined") {
       instanceHero();
     }
   });
