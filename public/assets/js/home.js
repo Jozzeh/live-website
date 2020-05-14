0eb6d71cabf1f5b2ac5705373496167545215d64
Jos De Berdt <josdeberdt@MacBook-Pro-van-Jos.local>
2020-05-14 22:08:34 +0200

    fix - dry

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -17,20 +17,8 @@ function instanceHero() {
 }
 
 window.onload = function() {
-  let hero = document.getElementById("heroContainer");
-  if(typeof hero !== "undefined" && hero !== null){
-    //hero exists
-    window.onscroll = function() {
-      let currentScrollPerc = ((window.scrollY / hero.offsetHeight * 100) / 2);
-      if(currentScrollPerc < 10){
-        if(currentScrollPerc > 10){
-          currentScrollPerc = 0;
-        }
-        let shiftLeft = currentScrollPerc + "%";
-        document.getElementById("heroLeft").style.left = "-" + shiftLeft;
-        document.getElementById("heroRight").style.left = shiftLeft;
-      }
-    }
+  if(typeof instanceHero !== "undefined") {
+    instanceHero();
   }
 };
 
