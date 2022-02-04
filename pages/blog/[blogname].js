34b09eb827c9319ea27dac9f453df8bcd0fed959
“Jozzeh” <jozzeh@gmail.com>
2022-02-04 12:59:52 +0100

    return to the old

--- pages/blog/[blogname].js
+++ pages/blog/[blogname].js
@@ -6,12 +6,8 @@ import Footer from "../../components/layouts/footer";
 import LineText from "../../components/layouts/linetext";
 
 import Article from "../../components/blocks/blog/article";
-import Articles from "../../data/blog/list.json";
-import ArticleDetails from "../../data/blog/articles.json";
 
-
-function Post(props) {
-  const articleContent = props.page;
+function Post({ articleContent }) {
   if (articleContent) {
     return (
       <>
@@ -64,18 +60,51 @@ function Post(props) {
   }
 }
 
-export async function getStaticPaths() {
-  const paths = Articles.data.map(page => {
-    const blogname = page.handle;
-    return {params: {blogname}};
-  });
-  return {paths, fallback: false};
-}
+// Post.getInitialProps = async (ctx) => {
+//   const articleData = articleList.data.filter(article => {
+//     if (article.handle === ctx.query.blogname) {
+//       return article;
+//     }
+//   });
 
-export async function getStaticProps({params}) {
-  var lastItem = params.blogname;
-  const page = ArticleDetails.data.find(page => page.handle === lastItem) || {notfound: true};
-  return {props: {page}};
+//   if (!articleData[0] || typeof articleData[0] === 'undefined') {
+//     return {
+//       error: {
+//         statusCode: 404
+//       }
+//     };
+//   }
+
+//   return {
+//     articleContent: articleData[0],
+//   };
+// };
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getServerSideProps(context) {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch(
+    process.env.BASE_URL +
+      "/data/blog/articles.json"
+  );
+  const articleList = await res.json();
+
+  const articleData = articleList.data.filter((article) => {
+    if (article.handle === context.query.blogname) {
+      return article;
+    }
+  });
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      articleContent: articleData[0],
+    },
+  };
 }
 
 export default Post;
